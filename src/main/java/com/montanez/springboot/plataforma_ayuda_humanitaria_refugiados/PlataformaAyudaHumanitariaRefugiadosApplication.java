07dcc11f6059fad4a21f536a60b07573193c745b
montanez8 <montanezcompany.r@gmail.com>
2024-04-02 07:33:15 -0500

    .

---
+++ src/main/java/com/montanez/springboot/plataforma_ayuda_humanitaria_refugiados/PlataformaAyudaHumanitariaRefugiadosApplication.java
@@ -0,0 +1,13 @@
+package com.montanez.springboot.plataforma_ayuda_humanitaria_refugiados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaAyudaHumanitariaRefugiadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaAyudaHumanitariaRefugiadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/montanez/springboot/plataforma_ayuda_humanitaria_refugiados/PlataformaAyudaHumanitariaRefugiadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.montanez.springboot.plataforma_ayuda_humanitaria_refugiados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaAyudaHumanitariaRefugiadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
