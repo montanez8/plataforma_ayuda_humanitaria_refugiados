80b634e4f589f5b49c1837ebbcc55a38a43d1f19
montanez8 <99758907+montanez8@users.noreply.github.com>
2024-04-10 19:00:35 -0500

    Añadidos arrancadores de Spring Boot para MongoDB y AMQP (RabbitMQ)
    Esta confirmación añade las siguientes dependencias al `pom.xml` del proyecto:
    * `spring-boot-starter-data-mongodb`.
    * spring-boot-starter-amqp
    Además, la clase `EnvioServiceImp` ha sido actualizada para utilizar el `RabbitTemplate` de RabbitMQ para enviar un `ShipmentCreatedEvent` cuando se crea un nuevo `Envio`. El `ShipmentCreatedEvent` también se guarda en MongoDB utilizando `MongoTemplate`.

--- src/main/java/com/montanez/springboot/plataforma_ayuda_humanitaria_refugiados/dto/dto_converter/dto_converter_informes/InformeSDtoConverter.java
+++ src/main/java/com/montanez/springboot/plataforma_ayuda_humanitaria_refugiados/dto/dto_converter/dto_converter_informes/InformeSDtoConverter.java
@@ -1,16 +1,14 @@
 package com.montanez.springboot.plataforma_ayuda_humanitaria_refugiados.dto.dto_converter.dto_converter_informes;
 
+import com.montanez.springboot.plataforma_ayuda_humanitaria_refugiados.dto.dto_informes.InformeSocioDto;
+import com.montanez.springboot.plataforma_ayuda_humanitaria_refugiados.repository.entities.Socio;
+import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
-import com.montanez.springboot.plataforma_ayuda_humanitaria_refugiados.dto.dto_informes.InformeSocioDto;
-import com.montanez.springboot.plataforma_ayuda_humanitaria_refugiados.repository.entities.Socio;
-
-import lombok.AllArgsConstructor;
-
 @Component
 @AllArgsConstructor
-public class InformeSocioDtoConverter {
+public class InformeSDtoConverter {
     private ModelMapper modelMapper;
 
     public InformeSocioDto convertToDto(Socio socio) {
--- src/main/java/com/montanez/springboot/plataforma_ayuda_humanitaria_refugiados/dto/dto_converter/dto_converter_informes/IsedeDtoConverter.java
+++ src/main/java/com/montanez/springboot/plataforma_ayuda_humanitaria_refugiados/dto/dto_converter/dto_converter_informes/IsedeDtoConverter.java
@@ -1,16 +1,14 @@
 package com.montanez.springboot.plataforma_ayuda_humanitaria_refugiados.dto.dto_converter.dto_converter_informes;
 
+import com.montanez.springboot.plataforma_ayuda_humanitaria_refugiados.dto.dto_informes.InformeSedeDto;
+import com.montanez.springboot.plataforma_ayuda_humanitaria_refugiados.repository.entities.Sede;
+import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
-import com.montanez.springboot.plataforma_ayuda_humanitaria_refugiados.dto.dto_informes.InformeSedeDto;
-import com.montanez.springboot.plataforma_ayuda_humanitaria_refugiados.repository.entities.Sede;
-
-import lombok.AllArgsConstructor;
-
 @Component
 @AllArgsConstructor
-public class InformeSedeDtoConverter {
+public class IsedeDtoConverter {
     private ModelMapper modelMapper;
 
     public InformeSedeDto convertToDto(Sede sede) {
